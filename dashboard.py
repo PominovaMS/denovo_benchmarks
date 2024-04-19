382dfb1223611ae1670c7e26161756f9c269f18c
m.pominova <m.pominova@haut.ai>
2024-04-19 14:39:22 +0200

    add dashborad script

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,10 @@
 import os
 import streamlit as st
+import streamlit.components.v1 as components
 
 RESULTS_DIR = "./results"
+PLOT_HEIGHT = 600
+PLOT_WIDTH = int(600 * 1.2)
 
 st.set_page_config(layout="wide")
 
@@ -19,10 +22,13 @@ st.subheader(dataset_name)
 col1, col2 = st.columns(2, gap="medium")
 
 with col1:
-    # st.write("Peptide precision & coverage")
-    st.image(os.path.join(RESULTS_DIR, "Peptide_precision_coverage.png"))
+    path_to_html = os.path.join(RESULTS_DIR, "Peptide_precision_coverage.html")
+    with open(path_to_html, 'r') as f: 
+        html_data = f.read()
+    components.html(html_data, width=PLOT_WIDTH, height=PLOT_HEIGHT, scrolling=False)
 
 with col2:
-    # st.write("Amino acid precision & coverage")
-    st.image(os.path.join(RESULTS_DIR, "AA_precision_coverage.png"))
-
+    path_to_html = os.path.join(RESULTS_DIR, "AA_precision_coverage.html")
+    with open(path_to_html, 'r') as f: 
+        html_data = f.read()
+    components.html(html_data, width=PLOT_WIDTH, height=PLOT_HEIGHT, scrolling=False)
