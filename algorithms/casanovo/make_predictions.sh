ebc07f1c8538e76be672c40118bd63aefb7f5914
PominovaMS <marinapoms@gmail.com>
2024-05-31 20:50:39 +0200

    update casanovo files with templates

--- algorithms/casanovo/make_predictions.sh
+++ algorithms/casanovo/make_predictions.sh
@@ -2,4 +2,4 @@
 # Run de novo algorithm on the input data
 casanovo sequence -c config.yml -o outputs.mztab "$@"/*.mgf
 # Convert predictions to the general output format
-python ./output_mapper.py outputs.mztab
+python ./output_mapper.py --output_path=outputs.mztab
