c982d05c801bd209f041544d8b2b0a7db4310731
Christian Nix <Christian-Nix@gmx.net>
2024-09-25 17:39:23 +0200

    Start on running casanovo

--- algorithms/spectralis/make_predictions.sh
+++ algorithms/spectralis/make_predictions.sh
@@ -1,1 +1,34 @@
-echo 'Spectralis ran'
+#!/bin/bash
+
+# Make pyenvs available
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+# Run Casanovo to obtain initial starting sequences
+pyenv activate casanovo
+casanovo_output="outputs.mztab"
+
+# Get dataset property tags
+DSET_TAGS=$(python /algo/base/dataset_tags_parser.py --dataset "$@")
+while IFS='=' read -r key value; do
+    export "$key"="$value"
+done <<< "$DSET_TAGS"
+
+# Use tag variables to specify de novo algorithm
+# for the particular dataset properties
+if  [[ -v nontryptic && $nontryptic -eq 1 ]]; then
+    # Run de novo algorithm on the input data
+    echo "Using non-tryptic model."
+    casanovo sequence -c casanovo_config.yml -o $casanovo_output "$@"/*.mgf --model ./casanovo_nontryptic.ckpt
+else
+    # Run de novo algorithm on the input data
+    echo "Using general model."
+    casanovo sequence -c casanovo_config.yml -o $casanovo_output "$@"/*.mgf
+fi
+
+# Change to spectralis pyenv
+pyenv deactivate
+pyenv activate spectralis
+
+# Write the initial starting sequences to the input MGFs
+python /algo/intermediate_mapper.py --mztab_path $casanovo_output --mgf_in_dir "$@"
