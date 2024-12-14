612d2561a44fa7dbf820468246b9c10aeb07fe4d
PominovaMS <marinapoms@gmail.com>
2024-12-14 14:05:27 +0100

    fix GCNovo input & output format

--- algorithms/gcnovo/make_predictions.sh
+++ algorithms/gcnovo/make_predictions.sh
@@ -10,11 +10,23 @@
 #done <<< "$DSET_TAGS"
 
 # Iterate through files in the dataset
-for input_file in "$@"/*.mgf; do
+mkdir -p /algo/input_data
+mkdir -p /algo/outputs
 
+for input_file in "$@"/*.mgf; do
     echo "Processing file: $input_file"
 
-     python gcnovo_main.py \
-        --denovo_input_spectrum_file "$input_file" \
-        --denovo_output_file="$input_file.csv"
+    # cp $input_file /algo/input_data/input_data.mgf
+    # Convert input data to model format
+    python input_mapper.py \
+        --input_path "$input_file" \
+        --output_path /algo/input_data/input_data.mgf
+
+    python gcnovo_main.py \
+        --denovo_input_spectrum_file="/algo/input_data/input_data.mgf" \
+        --denovo_input_feature_file="/algo/input_data/input_data.mgf.csv" \
+        --denovo_output_file="/algo/outputs/output"
+
+    # Collect predictions
+    cat "/algo/outputs/output[benchmark].csv" >> /algo/outputs.csv
 done
